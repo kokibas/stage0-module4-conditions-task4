b6173f9cc89228903f0f840aebeb7b4be9cefd67
kokibas <nurym0807@gmail.com>
2022-11-02 16:38:16 +0600

    check0

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+       int result = first >= second ? first : second;
+        int result2 = second >= third ? second : third;
+        if (result2 >= result ){
+            System.out.println(result2);
+        } else if (result2 <= result ) {
+            System.out.println(result);
+        }
+
+
+
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        System.out.println(first >= second ? first : second >= third ? second :third >= first ? third : first);
+//        int result2 = second >= third ? second : third;
+//        if (result2 >= result ){
+//            System.out.println(result2);
+//        } else if (result2 <= result ) {
+//            System.out.println(result);
+//        }
     }
 }
